09cff47b16a14ce89d9a0c9ad1da0f782173b0f6
JorgeArruda <arrudajorge49@gmail.com>
2018-05-13 23:11:06 +0000

    Dithering random ok

--- atividade05.py
+++ atividade05.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import numpy
 from cv2 import cv2
-import math
+import random
 
 foto_01 = cv2.imread('Imagens/foto01.png', cv2.IMREAD_GRAYSCALE)
 foto_02 = cv2.imread('Imagens/foto02.png')
@@ -16,12 +16,7 @@ girl = cv2.imread('Imagens/foto02.png')
 girl_gray = cv2.imread('Imagens/foto02.png', cv2.IMREAD_GRAYSCALE)
 
 def dithering_basico(image):
-    print("Result dimensions ",image.shape)
-    #a = np.array([1, 4, 5, 8], float)
-    #a = a.reshape((5, 2))
-    print(" Imagem ", image.shape)
-    # image = addZero(image)
-    # print("\n Imagem(addZeros) ", image.shape)
+    print("Image dimensions ",image.shape)
     
     if len(image.shape) == 2:
         result = numpy.zeros((image.shape[0],image.shape[1]), numpy.uint8)
@@ -36,11 +31,35 @@ def dithering_basico(image):
                     result[linha,coluna,canal] = 0 if image[linha,coluna,canal] <= 127 else 255
     return result
 
-if (__name__=='__main__'):
+def dithering_random(image, rangee=(-60,60)):
+    print("Image dimensions ",image.shape)
+    
+    if len(image.shape) == 2:
+        result = numpy.array(image)
+        for linha in range(1,image.shape[0]):
+            for coluna in range(1,image.shape[1]):
+                result[linha,coluna] = 0 if (image[linha,coluna]+random.randrange(rangee[0], rangee[1])) <= 127 else 255
+    else:
+        result = numpy.zeros((image.shape[0],image.shape[1],image.shape[2]), numpy.uint8)
+        for linha in range(1,image.shape[0]):
+            for coluna in range(1,image.shape[1]):
+                for canal in range(0,image.shape[2]):
+                    result[linha,coluna,canal] = 0 if (image[linha,coluna,canal]+random.randrange(rangee[0], rangee[1])) <= 127 else 255
+    return result
+
+if (__name__=='__main__c'):
     image = girl
     cv2.imshow('Image', image)
     cv2.imshow('Dithering basico', dithering_basico(image))
     image = girl_gray
     cv2.imshow('Image grayscale', image)
     cv2.imshow('Dithering basico - grayscale', dithering_basico(image))
+    cv2.waitKey(0)
+if (__name__=='__main__'):
+    image = girl
+    cv2.imshow('Image', image)
+    cv2.imshow('Dithering random', dithering_random(image))
+    image = girl_gray
+    cv2.imshow('Image grayscale', image)
+    cv2.imshow('Dithering random - grayscale', dithering_random(image))
     cv2.waitKey(0)
