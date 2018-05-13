61f1278bf23b310e898a87efb8777c0083c79074
JorgeArruda <arrudajorge49@gmail.com>
2018-05-13 01:18:17 +0000

    Inicio do realce de bordas

---
+++ atividade04.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+# Atividade 02 Histograma
+import numpy
+import cv2
+
+# Atividade 04 Realce bordas:
+#     - Sobel          
+#     - Prewitt           
+#     - Roberts            
+#     - Isotrópico 
